5e994d4c66585ab34bdc58344063ae04d1bbcffd
Kevin Rivera <shimpo11@outlook.com>
2022-11-21 17:34:49 -0500

    Angular proyecto
    - Agregando CDN de iconos
    - Maquetando Dashboard html y css
    - Modificando imagen de perfil de usuario
    - Creacion  de  componentes Catalogo y producto
    - Maquetado de Login html y css
    - Definicion de rutas

---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { UserComponent } from './user/user/user.component';
+import { HeaderComponent } from './static/header/header.component';
+import { NavComponent } from './static/nav/nav.component';
+import { FooterComponent } from './static/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+
+import { BreadcrumbsComponent } from './static/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './static/sidebar/sidebar.component';
+
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { ProductComponent } from './pages/product/product.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    UserComponent,
+
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    NotFoundComponent,
+    CatalogueComponent,
+    ProductComponent,
+    PagesComponent,
+    LoginComponent,
+    RegisterComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
